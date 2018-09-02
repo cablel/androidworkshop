bf2ecfd42698a80dbb16bb5062d59f40ad18a059
charuwit <charuwit.tararphon@gmail.com>
2018-09-02 15:33:34 +0700

    first commit

---
+++ app/src/main/java/com/example/charu/myapplication/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.charu.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+
+public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.tvMessage) TextView tvMessage;
+//
+    @OnClick(R.id.button2)
+    public void doClickHello(){
+        this.tvMessage.setText(R.string.helloMessage);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+//        final TextView tvMessage = findViewById(R.id.tvMessage);
+//
+//        Button button2 = (Button) findViewById(R.id.button2);
+//        button2.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                tvMessage.setText("Hello John");
+//            }
+//        });
+
+    }
+
+}
